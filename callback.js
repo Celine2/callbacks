3c3de60bde125d0b5ecd79e1a3fc94a6aaf03664
tonitoelgalo <adirat@free.fr>
2017-12-18 18:00:03 +0100

    Add files via upload

--- callback.js
+++ callback.js
@@ -1,13 +1,1 @@
-function add(x,y, callback){
-	var result = x + y;
 
-	callback(result);
-}
-
-function showResult(result){
-	alert(result);
-}
-
-$("#foo").click(function(){
-	add(3, 5, showResult);
-});
